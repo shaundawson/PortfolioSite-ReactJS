12825c86e2e446978755587b408f1ec94c53775b
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-06 12:29:37 -0500

    created About component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header/>
+       <About />
       </div>
     );
   }
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,1 +1,29 @@
-header.js
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+      return (
+
+        <section id="about">
+        <div className="row">
+          <div className="two columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt />
+          </div>
+          <div className="ten columns main-col">
+            <h2>Let me introduce myself.</h2>
+            <p>Lorem Ipsum
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+            "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            </p>
+              <div className="twelve columns main-col">
+                <p>
+                  <a href="#" className="button"><i className="fa fa-download" />Download Resume</a>
+                </p>
+              </div>
+            </div> {/* end row */}
+          </div> {/* end .main-col */}
+      </section> 
+    );
+  }
+};
