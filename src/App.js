bf47cf17d421274cf14fd9d79311ce19a4fdf740
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-06 12:36:27 -0500

    created Porfolio component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
        <Header/>
        <About />
+       <Portfolio />
       </div>
     );
   }
