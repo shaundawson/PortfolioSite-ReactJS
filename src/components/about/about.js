c8d1c1feeac62b613f5d55b0219e009f775484f0
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-13 12:44:37 -0500

    updated header, about and portfolio component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,15 +6,10 @@ export default class About extends Component {
 
         <section id="about">
         <div className="row">
-          <div className="two columns">
-            <img className="profile-pic" src="images/profilepic.jpg" alt />
-          </div>
-          <div className="ten columns main-col">
-            <h2>Let me introduce myself.</h2>
-            <p>Lorem Ipsum
-            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-            "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          <div className="twelve columns main-col">
+          <h2>about.</h2>
+          <img className="profile-pic" src="images/profilepic.jpg" alt />
+            <p> <b>Full Stack Developer | Documentary Filmmaker </b><br></br> I'm a design oriented programmer with a rich blend of experience in new media. I'm passionate about telling stories that amplify the voices of individuals who are perceived by the majority as being different in some fundamental way. 
             </p>
               <div className="twelve columns main-col">
                 <p>
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -7,7 +7,9 @@ export default class Portfolio extends Component {
         <section id="portfolio">
         <div className="row">
           <div className="twelve columns collapsed">
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Check out my work.</h1>
+            <h6>bytes of code.</h6>
+            <br></br>
             {/* portfolio-wrapper */}
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
               <div className="columns portfolio-item">
@@ -16,8 +18,8 @@ export default class Portfolio extends Component {
                     <img alt src="images/portfolio/coffee.jpg" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Coffee</h5>
-                        <p>Illustrration</p>
+                        <h5>docupicks.com</h5>
+                        <p>Web Design & Development</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
@@ -30,8 +32,8 @@ export default class Portfolio extends Component {
                     <img alt src="images/portfolio/console.jpg" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Console</h5>
-                        <p>Web Development</p>
+                        <h5>blackfriendsdinner.com</h5>
+                        <p>Web Design & Development</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
@@ -44,8 +46,8 @@ export default class Portfolio extends Component {
                     <img alt src="images/portfolio/judah.jpg" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Judah</h5>
-                        <p>Webdesign</p>
+                        <h5>devjobtools.com</h5>
+                        <p>Web Design & Development</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
@@ -58,14 +60,18 @@ export default class Portfolio extends Component {
                     <img alt src="images/portfolio/into-the-light.jpg" />
                     <div className="overlay">
                       <div className="portfolio-item-meta">
-                        <h5>Into The Light</h5>
-                        <p>Photography</p>
+                        <h5>momoya</h5>
+                        <p>Web Development</p>
                       </div>
                     </div>
                     <div className="link-icon"><i className="icon-plus" /></div>
                   </a>
                 </div>
               </div> {/* item end */}
+
+              <hr></hr>
+              <h6> documentaries.</h6>
+              <br></br>
               <div className="columns portfolio-item">
                 <div className="item-wrap">
                   <a href="#modal-05" title>
