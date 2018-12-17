7d76230873d9d39e6986030e733cdab35806269d
Shaun Dawson <shaundawson0883@gmail.com>
2018-12-17 13:39:17 -0500

    update contact component

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -11,15 +11,15 @@ export default class About extends Component {
               <fieldset>
                 <div>
                   <label htmlFor="contactName">Name <span className="required">*</span></label>
-                  <input type="text" defaultValue size={35} id="contactName" name="contactName" defaultValue={""} />
+                  <input type="text"  size={35} id="contactName" name="contactName" defaultValue={""} />
                 </div>
                 <div>
                   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                  <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" defaultValue={""} />
+                  <input type="text" size={35} id="contactEmail" name="contactEmail" defaultValue={""} />
                 </div>
                 <div>
                   <label htmlFor="contactSubject">Subject</label>
-                  <input type="text" defaultValue size={35} id="contactSubject" name="contactSubject" defaultValue={""} />
+                  <input type="text" size={35} id="contactSubject" name="contactSubject" defaultValue={""} />
                 </div>
                 <div>
                   <label htmlFor="contactMessage">Message <span className="required">*</span></label>
