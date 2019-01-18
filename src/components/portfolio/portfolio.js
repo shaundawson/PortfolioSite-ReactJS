7c3d136c758cf339fc601defb8fd2389c8a8272d
Shaun Dawson <shaundawson0883@gmail.com>
2019-01-17 20:53:06 -0800

    theme update - portfolio

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -70,8 +70,8 @@ export default class Portfolio extends Component {
           <div id="modal-01" className="popup-modal mfp-hide">
             <img className="scale-with-grid" src="images/portfolio/modals/m-blackfriendsdinner.jpg" alt />
             <div className="description-box">
-              <h4>Black Friends Dinner Podcast</h4>
-              <p>Where we navigate what #AllBlackLivesMatter means in a world where they don't, over food and drink made with love by NandiKayyy, Your resident Black Non-Binary Bae. Available on Spotify, Google Play Music and Itunes. </p>
+              <h4>Black Friends Dinner Website</h4>
+              <p>Black Friends Dinner is a table side conversation where guests navigate what it means to value Black lives in a world that doesn't. </p>
               <span className="categories"><i className="fa fa-tag" />Audio Production, Web Design, Web Developement</span>
             </div>
             <div className="link-box">
@@ -79,42 +79,8 @@ export default class Portfolio extends Component {
               <a className="popup-modal-dismiss">Close</a>
             </div>
           </div>{/* modal-01 End */}
-          <div id="modal-02" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-console.jpg" alt />
-            <div className="description-box">
-              <h4>Dev Job Hunter</h4>
-              <p>DevJobHunter is a platform for developers to find employment based on location. </p>
-              <span className="categories"><i className="fa fa-tag" /> Web Design, Web Development</span>
-            </div>
-            <div className="link-box">
-              <a href="http://www.devjobhunter.com" target="_blank" rel="noopener noreferrer" >Visit Website</a>
-              <a className="popup-modal-dismiss">Close</a>
-            </div>
-          </div>{/* modal-02 End */}
-          <div id="modal-03" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-judah.jpg" alt />
-            <div className="description-box">
-              <h4>Reach App</h4>
-              <p>Reach is a digital platform where tech brands collaborate with impactful Youtube influencers to expand their brand reach.</p>
-              <span className="categories"><i className="fa fa-tag" />Web Design, Web Development</span>
-            </div>
-            <div className="link-box">
-              <a href="#">Visit Website</a>
-              <a className="popup-modal-dismiss">Close</a>
-            </div>
-          </div>{/* modal-03 End */}
-          <div id="modal-04" className="popup-modal mfp-hide">
-            <img className="scale-with-grid" src="images/portfolio/modals/m-intothelight.jpg" alt />
-            <div className="description-box">
-              <h4>Docupicks </h4>
-              <p>The docupicks app provides reviews, youtbe trailers and streaming sources for IMDB's top rated documentary films and TV series. Users also have the ability to create a "watch-list" and provide feedback to the community about films they have watched in the past.</p>
-              <span className="categories"><i className="fa fa-tag" />Web Design, Web Development</span>
-            </div>
-            <div className="link-box">
-              <a href="#">Visit Website</a>
-              <a className="popup-modal-dismiss">Close</a>
-            </div>
-          </div>{/* modal-04 End */}
+
+        
           <div id="modal-05" className="popup-modal mfp-hide">
             <img className="scale-with-grid" src="images/portfolio/modals/m-nandikayyy.png" alt />
             <div className="description-box">
